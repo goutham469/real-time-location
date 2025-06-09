88a398125d065357972a9d6b3c4296740b74ab59
goutham <uppinurigouthamreddy@gmail.com>
2025-06-09 18:56:13 +0530

    server added

---
+++ server/index.js
@@ -0,0 +1,57 @@
+const express = require("express")
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+
+const server = http.createServer( app );
+
+const io = new Server( server , {
+    cors:{ origin:"*" }
+} )
+
+app.use(cors())
+app.get("/" , (req,res)=>{
+    res.send("Server running ... ")
+})
+
+app.post("/auth" , (req,res) => {
+    console.log(req.body);
+
+    try{
+        const { username , password } = req.body;
+
+
+    }catch(err){
+        res.send({
+            success:false,
+            error:err.message
+        })
+    }
+} )
+
+const userLocations = []
+
+io.on( "connection" , (socket) => {
+    console.log("new connection");
+    
+    socket.on("updateLocation", (data) => {
+    const index = userLocations.findIndex((u) => u.id === socket.id);
+    if (index === -1) {
+      userLocations.push({ id: socket.id, ...data });
+    } else {
+      userLocations[index] = { id: socket.id, ...data };
+    }
+
+    io.emit("locations", userLocations.filter((u) => u.id !== socket.id));
+  });
+
+  socket.on("disconnect", () => {
+    userLocations = userLocations.filter((u) => u.id !== socket.id);
+    io.emit("locations", userLocations);
+  });
+
+});
+
+server.listen(5000, () => console.log("Server started on http://localhost:5000"));
